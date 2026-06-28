8e004a3a77889e3db7e4f3545fc80b9b1010f51f
Pole <pole97@live.it>
2026-06-28 19:41:17 +0200

    addAsta api

--- client/src/entities/player.ts
+++ client/src/entities/player.ts
@@ -1,5 +1,35 @@
 class Player {
+    id: number;
+    asta_id: number;
+    name: string;
+    team: string;
+    role: string;
+    index_role: string;
+    role_mantra: string;
+    notes: string;
+    taken: number;
 
+    constructor (
+        id: number,
+        asta_id: number,
+        name: string,
+        team: string,
+        role: string,
+        index_role: string,
+        role_mantra: string,
+        notes: string,
+        taken: number
+    ) {
+        this.id = id;
+        this.asta_id = asta_id;
+        this.name = name;
+        this.team = team;
+        this.role = role;
+        this.index_role = index_role;
+        this.role_mantra = role_mantra;
+        this.notes = notes;
+        this.taken = taken;
+    }
 }
 
 export default Player;
